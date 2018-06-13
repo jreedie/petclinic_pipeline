18476217ebe4648d84327a5233131f587e6a7273
Mike Gajda <gajda@galatea-associates.com>
2018-06-13 16:59:15 -0400

    gets mvn test working

--- cucumber_resources/src/test/java/cuketest/RunCukesTest.java
+++ cucumber_resources/src/test/java/cuketest/RunCukesTest.java
@@ -4,13 +4,17 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "src/test/resources/cuketest")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features = "src/test/resources/cuketest",
+	glue = "src/test/groovy",
+	plugin = "json:target/cucumber_output.json"
+	)
 public class RunCukesTest {
-	public static void main(String[] args) throws Throwable {
-    //String[] arguments = {"--glue", "src/test/groovy", "src/test/resources"};
-    cucumber.api.cli.Main.main(args);
-	}
+	// public static void main(String[] args) throws Throwable {
+ //    //String[] arguments = {"--glue", "src/test/groovy", "src/test/resources"};
+ //    cucumber.api.cli.Main.main(args);
+	// }
 }
 
 
